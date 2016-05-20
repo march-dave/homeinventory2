9f826dae86e1e9d307521afa631cf50b5cc6db86
march-dave <ysangy@gmail.com>
2016-05-19 17:02:17 -0700

    clean db connection

--- config/db.js
+++ config/db.js
@@ -3,10 +3,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 'g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user     : 'q6w1bky95fz8nh2p',
-  password : 'tow14ccx0eb9dzsg',
-  database : 'kitefin'
+  host     : 'localhost',
+  user     : 'root',
+  password : '1234',
+  database : 'testdb'
 });
 
 connection.connect();
