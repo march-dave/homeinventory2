72cc7debe403482499b4b95a5c455b1391886486
march-dave <ysangy@gmail.com>
2016-05-19 17:31:28 -0700

    db connection update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var connection = mysql.createConnection(process.env.JASWDB_URL || {
   host     : 'g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   user     : 'q6w1bky95fz8nh2p',
   password : 'tow14ccx0eb9dzsg',
